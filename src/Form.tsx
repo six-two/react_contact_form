22500a925876fc02ab4550a3e17d04eec1cb6eb0
six-two <git@six-two.dev>
2021-01-04 11:06:18 +1300

    Small refactoring for Form

--- src/Form.tsx
+++ src/Form.tsx
@@ -2,6 +2,8 @@ import { useState } from "react";
 import { submitForm } from "./Submit";
 import { validate } from "./Validator";
 
+const TEXT_AREA_PLACEHOLDER = "You can write this message in English or German. If you need to send me a file, upload it somewhere (Dropbox, Mega, etc) and just put the link here.";
+
 export interface MyFormData {
     name: string,
     email: string,
@@ -17,6 +19,19 @@ export const DEFAULT_FORM_DATA: MyFormData = {
 interface Props {
 }
 
+const trimFormData = (data: MyFormData): MyFormData => {
+    return {
+        name: data.name.trim(),
+        email: data.email.trim(),
+        message: data.message.trim(),
+    }
+}
+
+const validateAndSubmit = (data: MyFormData) => {
+    data = trimFormData(data);
+    validate(data).then((valid) => valid && submitForm(data));
+}
+
 const Form = (props: Props) => {
     const [data, setData] = useState(DEFAULT_FORM_DATA);
     const onChange = (fieldName: string) => {
@@ -30,22 +45,14 @@ const Form = (props: Props) => {
             }
         }
     };
-    const submit = () => {
-        const trimmed_data: MyFormData = {
-            name: data.name.trim(),
-            email: data.email.trim(),
-            message: data.message.trim(),
-        }
-        validate(trimmed_data).then((valid) => valid && submitForm(trimmed_data));
-    }
     return <div className="form">
         <h2>Name</h2>
         <input id="name" type="text" placeholder="Optional" onChange={onChange("name")} />
         <h2>Email</h2>
-        <input id="email" type="text" placeholder="Required for me to respond" onChange={onChange("email")} />
+        <input id="email" type="text" placeholder="Where to send my response" onChange={onChange("email")} />
         <h2>Message</h2>
-        <textarea placeholder="You can write this message in English or German. If you need to send me a file, upload it somewhere (Dropbox, Mega, etc) and just put the link here." onChange={onChange("message")} />
-        <button onClick={submit}>Send message</button>
+        <textarea placeholder={TEXT_AREA_PLACEHOLDER} onChange={onChange("message")} />
+        <button onClick={() => validateAndSubmit(data)}>Send message</button>
     </div>
 }
 
